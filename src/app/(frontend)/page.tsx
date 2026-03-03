42bb2e37f3b02e99c1f729839833a981695d2a7f
mishakahan <kahan.jonathan@gmail.com>
2026-03-03 23:44:16 +0100

    Update page.tsx

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function HomePage() {
-  return (
-    <iframe
-      src="/box.html"
-      style={{ width: '100%', height: '100vh', border: 'none' }}
-    />
-  )
+  redirect('/box.html')
 }
